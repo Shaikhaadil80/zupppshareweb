d4bb861ee8a487fb6f4b5f324b3347d43b3a720a
yewtecaadil <yewtec.aadil.outlook.com>
2025-07-06 13:37:38 +0530

    url changes with build

---
+++ assets/public/as.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class asd extends StatelessWidget {
+  const asd({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: ElevatedButton(child: Text('data'),onPressed: (){}),);
+  }
+}
